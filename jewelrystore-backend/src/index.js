e4f58cb3ab8484a885b2cdc7cef6a70f33e731e9
pnQuanq <phamquang13103@gmail.com>
2024-05-05 15:58:57 +0700

    Update-User

--- jewelrystore-backend/src/index.js
+++ jewelrystore-backend/src/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const bodyParser = require("body-parser");
 
-const app = express();
-const port = process.env.PORT || 3011;
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+dotenv.config()
 
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.json())
+
+routes(app);
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+    .then(() => {
+        console.log('Connect Db success!')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 app.listen(port, () => {
-  console.log("Server is running in port " + port);
-});
+    console.log('Server is running in port: ', + port)
+})
