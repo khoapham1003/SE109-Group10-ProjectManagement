38d6232ec458e8902d676f2878fb79c7cf5d9864
pnQuanq <phamquang13103@gmail.com>
2024-05-16 08:48:31 +0700

    update

--- jewelrystore-backend/src/services/ProductService.js
+++ jewelrystore-backend/src/services/ProductService.js
@@ -124,7 +124,7 @@ const getDetailsProduct = (id) => {
 const getAllProduct = (limit, page, sort, filter) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const totalProduct = await Product.count()
+            /* const totalProduct = await Product.count()
             let allProduct = []
             if (filter) {
                 const label = filter[0];
@@ -155,14 +155,15 @@ const getAllProduct = (limit, page, sort, filter) => {
                 allProduct = await Product.find().sort({createdAt: -1, updatedAt: -1})
             }else {
                 allProduct = await Product.find().limit(limit).skip(page * limit).sort({createdAt: -1, updatedAt: -1})
-            }
+            } */
+            const allProduct = await Product.find()
             resolve({
                 status: 'OK',
                 message: 'Success',
                 data: allProduct,
-                total: totalProduct,
+                /* total: totalProduct,
                 pageCurrent: Number(page + 1),
-                totalPage: Math.ceil(totalProduct / limit)
+                totalPage: Math.ceil(totalProduct / limit) */
             })
         } catch (e) {
             reject(e)
